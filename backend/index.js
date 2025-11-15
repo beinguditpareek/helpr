64fa0a23422633e84a09ce3c823b58f00a94f045
sagar <sagarmalyadav0609@gmail.com>
2025-11-15 11:08:52 +0530

    'register'

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,25 @@
+// const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import dbconnection from './src/config/db.js'
+import userRouter from './src/routes/userRoute.js'
+// import bodyParser from 'body-parser'
+const app = express()
+dotenv.config()
+// app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
+
+dbconnection()
+
+app.use("/help", userRouter)
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+    
+})
