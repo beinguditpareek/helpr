32a0fed059ea30b5b8dbd1c52d56a7970743edb8
sagar <sagarmalyadav0609@gmail.com>
2025-11-17 12:09:59 +0530

    login api

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import {registerUser} from '../controller/userController.js'
+import {getAlluser, loginUser, registerUser} from '../controller/userController.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register',registerUser)
+userRouter.get('/getUser',getAlluser)
+userRouter.post('/loginUser',loginUser)
 
 export default userRouter;
